a249ff9b39a69e965b3541abeb88cdd4bbe14d0b
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2026-02-03 14:00:52 +0530

    new file:   .gitignore 	new file:   components.json 	new file:   drizzle.config.ts 	new file:   eslint.config.mjs 	new file:   next.config.ts 	new file:   package.json 	new file:   pnpm-lock.yaml 	new file:   pnpm-workspace.yaml 	new file:   postcss.config.mjs 	new file:   public/file.svg 	new file:   public/globe.svg 	new file:   public/next.svg 	new file:   public/vercel.svg 	new file:   public/window.svg 	new file:   src/app/favicon.ico 	new file:   src/app/globals.css 	new file:   src/app/layout.tsx 	new file:   src/app/page.tsx 	new file:   src/components/ui/accordion.tsx 	new file:   src/components/ui/alert-dialog.tsx 	new file:   src/components/ui/alert.tsx 	new file:   src/components/ui/aspect-ratio.tsx 	new file:   src/components/ui/avatar.tsx 	new file:   src/components/ui/badge.tsx 	new file:   src/components/ui/breadcrumb.tsx 	new file:   src/components/ui/button-group.tsx 	new file:   src/components/ui/button.tsx 	new file:   src/components/ui/calendar.tsx 	new file:   src/components/ui/card.tsx 	new file:   src/components/ui/carousel.tsx 	new file:   src/components/ui/chart.tsx 	new file:   src/components/ui/checkbox.tsx 	new file:   src/components/ui/collapsible.tsx 	new file:   src/components/ui/combobox.tsx 	new file:   src/components/ui/command.tsx 	new file:   src/components/ui/context-menu.tsx 	new file:   src/components/ui/dialog.tsx 	new file:   src/components/ui/direction.tsx 	new file:   src/components/ui/drawer.tsx 	new file:   src/components/ui/dropdown-menu.tsx 	new file:   src/components/ui/empty.tsx 	new file:   src/components/ui/field.tsx 	new file:   src/components/ui/form.tsx 	new file:   src/components/ui/hover-card.tsx 	new file:   src/components/ui/input-group.tsx 	new file:   src/components/ui/input-otp.tsx 	new file:   src/components/ui/input.tsx 	new file:   src/components/ui/item.tsx 	new file:   src/components/ui/kbd.tsx 	new file:   src/components/ui/label.tsx 	new file:   src/components/ui/menubar.tsx 	new file:   src/components/ui/native-select.tsx 	new file:   src/components/ui/navigation-menu.tsx 	new file:   src/components/ui/pagination.tsx 	new file:   src/components/ui/popover.tsx 	new file:   src/components/ui/progress.tsx 	new file:   src/components/ui/radio-group.tsx 	new file:   src/components/ui/resizable.tsx 	new file:   src/components/ui/scroll-area.tsx 	new file:   src/components/ui/select.tsx 	new file:   src/components/ui/separator.tsx 	new file:   src/components/ui/sheet.tsx 	new file:   src/components/ui/sidebar.tsx 	new file:   src/components/ui/skeleton.tsx 	new file:   src/components/ui/slider.tsx 	new file:   src/components/ui/sonner.tsx 	new file:   src/components/ui/spinner.tsx 	new file:   src/components/ui/switch.tsx 	new file:   src/components/ui/table.tsx 	new file:   src/components/ui/tabs.tsx 	new file:   src/components/ui/textarea.tsx 	new file:   src/components/ui/toggle-group.tsx 	new file:   src/components/ui/toggle.tsx 	new file:   src/components/ui/tooltip.tsx 	new file:   src/db/index.ts 	new file:   src/db/schema.ts 	new file:   src/hooks/use-mobile.ts 	new file:   src/lib/auth.ts 	new file:   src/lib/utils.ts 	new file:   tsconfig.json

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button>
+
+      Home
+      </Button>
+    </div>
+  )
+}
+
+export default HomePage
